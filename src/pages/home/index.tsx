d1138921c715eaa1d5818e0e9b4b83fb0a339483
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2023-01-09 12:08:50 +0000

    Estrutura visual da home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,25 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+import imagePreview from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size='4xl'>Agendamento descomplicado</Heading>
+        <Text size='lg'>Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.</Text>
+      </Hero>
+
+      <Preview>
+        <Image 
+          src={imagePreview}
+          height={400}
+          quality={100}
+          priority
+          alt='Clendário simbolizando aplicação em funcionamento'
+        />
+      </Preview>
+    </Container>
+  )
+}
