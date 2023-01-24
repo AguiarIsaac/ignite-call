7e3ac8564644a9ac6dccca73450513cee1abcc2a
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2023-01-24 20:12:23 +0000

    Validação e transformação no Zod

--- src/pages/register/time-intervals/styles.ts
+++ src/pages/register/time-intervals/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@ignite-ui/react'
+import { Box, styled, Text } from '@ignite-ui/react'
 
 export const IntervalBox = styled(Box, {
   marginTop: '$6',
@@ -38,4 +38,9 @@ export const IntervalInputs = styled('div', {
     // PARA ESTILIZAR ICONES DE DENTRO DOS INPUTS. SÓ FUNCIONA VIA FILTER, BACKGROUND E COLOCAR NÃO FUNCIONA
     filter: 'invert(100%) brightness(40%)',
   },
+})
+
+export const FormError = styled(Text, {
+  color: '#f75a68',
+  marginBottom: '$4',
 })
