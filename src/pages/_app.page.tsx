660e5df1a1842d3d51b8d34e9432435b9d99ccfa
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2023-02-08 18:48:23 +0000

    Configurando React Query

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,13 +2,17 @@ import type { AppProps } from 'next/app'
 import { GlobalStyle } from '../styles/global'
 import { SessionProvider } from 'next-auth/react'
 import '../lib/dayjs'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../lib/react-query'
 
 GlobalStyle()
 
 export default function App({ Component, pageProps: { session, ...pageProps }, }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
