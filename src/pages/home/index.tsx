8fbdac77b29273eea0feb63b6c4cf12957cc63aa
AguiarIsaac <isaacaguiar_@hotmail.com>
2023-02-12 09:05:29 -0300

    Otimizações de SEO

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,25 +3,32 @@ import Image from 'next/image'
 import { Container, Hero, Preview } from './styles'
 import imagePreview from '../../assets/app-preview.png'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo/lib/meta/nextSEO'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size='4xl'>Agendamento descomplicado</Heading>
-        <Text size='lg'>Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.</Text>
-        <ClaimUsernameForm />
-      </Hero>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
+      <Container>
+        <Hero>
+          <Heading size='4xl'>Agendamento descomplicado</Heading>
+          <Text size='lg'>Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.</Text>
+          <ClaimUsernameForm />
+        </Hero>
 
-      <Preview>
-        <Image 
-          src={imagePreview}
-          height={400}
-          quality={100}
-          priority
-          alt='Clendário simbolizando aplicação em funcionamento'
-        />
-      </Preview>
-    </Container>
+        <Preview>
+          <Image 
+            src={imagePreview}
+            height={400}
+            quality={100}
+            priority
+            alt='Clendário simbolizando aplicação em funcionamento'
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
